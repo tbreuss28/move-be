a20b731db5942442a9edd330c14f6134cd7f5d01
Dominik Mlasko <dominik.mlasko@blum.com>
2021-07-02 22:08:05 +0200

    Add Entity Controller and H2 Database entity

--- src/main/java/com/movenow/movenow/domain/Move.java
+++ src/main/java/com/movenow/movenow/domain/Move.java
@@ -1,8 +1,39 @@
 package com.movenow.movenow.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "move")
 public class Move {
+	
+	@Id
+    @GeneratedValue
+	private Long id;
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	private String name;
 
 }
