29d0b3def915d0a8dd8d57c60911825dc6d4b44b
Matthias Winkler <matthias.winkler@aleas.li>
2021-07-02 22:30:22 +0200

    Add move properties

--- src/main/java/com/movenow/movenow/domain/Move.java
+++ src/main/java/com/movenow/movenow/domain/Move.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.time.DateTimeException;
+import java.util.Date;
 
 @Entity
 @Table(name = "move")
@@ -12,28 +14,74 @@ public class Move {
 	@Id
     @GeneratedValue
 	private Long id;
+
+	private String name;
+
+	private String Description;
+	
+	private Date StartTime;
+	
+	private Date EndTime;
+	
+	private Double Latitude;
+	
+	private Double Longitude;
 	
 	
 	public Long getId() {
 		return id;
 	}
 
-
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
 
-	private String name;
+	public String getDescription() {
+		return Description;
+	}
 
+	public void setDescription(String description) {
+		Description = description;
+	}
+
+	public Date getStartTime() {
+		return StartTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		StartTime = startTime;
+	}
+
+	public Date getEndTime() {
+		return EndTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		EndTime = endTime;
+	}
+
+	public Double getLatitude() {
+		return Latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		Latitude = latitude;
+	}
+
+	public Double getLongitude() {
+		return Longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		Longitude = longitude;
+	}
 }
