cabc070da0e95a90749446dda5f1d23ba92b5f46
Matthias Winkler <matthias.winkler@aleas.li>
2021-07-02 22:11:15 +0200

    Add user entity

--- src/main/java/com/movenow/movenow/application/UserController.java
+++ src/main/java/com/movenow/movenow/application/UserController.java
@@ -1,5 +1,8 @@
 package com.movenow.movenow.application;
 
+import com.movenow.movenow.domain.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +19,12 @@ public class UserController {
 	
 	@GetMapping("/users")
 	@ResponseBody
-	  public String sayHello() {
+	  public ResponseEntity<User> sayHello() {
+		var user = new User();
 		
+		user.setUserName("kirbby");
 		
-		
-		return "Hellooooo User";
+		return new ResponseEntity<>(user, HttpStatus.OK);
 	  }
 
 }
---
+++ src/main/java/com/movenow/movenow/domain/User.java
@@ -0,0 +1,52 @@
+package com.movenow.movenow.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.lang.reflect.Field;
+
+@Entity(name="user")
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+    
+    private String lastName;
+    
+    private String userName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
