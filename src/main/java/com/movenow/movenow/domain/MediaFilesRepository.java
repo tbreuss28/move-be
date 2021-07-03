2bae3fcba7b5c23c840552c4b94a73a1285b92ec
Matthias Winkler <matthias.winkler@aleas.li>
2021-07-03 14:38:49 +0200

    Add MediaFile

---
+++ src/main/java/com/movenow/movenow/domain/MediaFilesRepository.java
@@ -0,0 +1,6 @@
+package com.movenow.movenow.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MediaFilesRepository extends JpaRepository<MediaFile, Long> {
+}
