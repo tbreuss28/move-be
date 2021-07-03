b05d1937bc8d56edf3c44751140a38f76d12c06a
Dominik Mlasko <dominik.mlasko@blum.com>
2021-07-03 17:18:49 +0200

    Refactor categories

--- src/main/java/com/movenow/movenow/application/SkillController.java
+++ src/main/java/com/movenow/movenow/application/SkillController.java
@@ -1,52 +1,44 @@
 package com.movenow.movenow.application;
 
 import com.movenow.movenow.domain.Skill;
+import com.movenow.movenow.domain.skill.SkillService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
+
+
+
+
 
 @RestController
 @RequestMapping("/api/skills")
 public class SkillController {
+	
+	@Autowired
+	SkillService skillService;
 
-    private List<Skill> skills  = new ArrayList<>();
 
-    @PostConstruct
-    private void postConstruct() {
-        skills.add(new Skill() {{
-            setId(1L);
-            setName("Anfänger");
-        }});
-        skills.add(new Skill() {{
-            setId(2L);
-            setName("Hobbysportler");
-        }});
-        skills.add(new Skill() {{
-            setId(3L);
-            setName("Profisportler");
-        }});
-    }
+ 
 
     @GetMapping
     public List<Skill> getSkills() {
-        return skills;
+        return skillService.getSkills();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity getSkill(@PathVariable Long id) {
-        for (Skill skill:
-                skills) {
-            if (skill.getId() == id) {
-                return ResponseEntity.ok(skill);
-            }
-        }
+    public ResponseEntity<Skill> getSkill(@PathVariable Long id) {
+    	
+    	try {
+    		return ResponseEntity.ok(skillService.getSkill(id));
+    	} catch(Exception e) {
+            return ResponseEntity.notFound().build();
+    	}        
 
-        return ResponseEntity.notFound().build();
     }
 }
